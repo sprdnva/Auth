e424815f673bc12088d93c6f5f16d79dbbce49ff
ulyana-spiridonova <ulyana.spiridonova@gmail.com>
2019-03-07 11:29:33 +0200

    setup mongodb

--- server/index.js
+++ server/index.js
@@ -3,7 +3,16 @@ const http = require("http");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
+const router = require("./router");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:auth/auth", { useNewUrlParser: true });
+
+app.use(morgan("combined"));
+app.use(bodyParser.json({ type: "*/*" }));
 
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
-server.listen(port, () => { console.log("listening on port", port) });
+server.listen(port, () => {
+  console.log("listening on port", port);
+});
