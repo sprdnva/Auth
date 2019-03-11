0790d1ec4b3bedae4d9b0a79235f82353d8551a8
ulyana-spiridonova <ulyana.spiridonova@gmail.com>
2019-03-11 14:48:14 +0200

    add jwt

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,4 +1,11 @@
 const User = require("../models/User");
+const jwt = require("jwt-simple");
+const config = require("../../config");
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = (req, res, next) => {
   const email = req.body.email;
@@ -25,7 +32,7 @@ exports.signup = (req, res, next) => {
       if (err) {
         return next(err);
       }
+      res.json({ token: tokenForUser(user) });
     });
-    res.json(user);
   });
 };
